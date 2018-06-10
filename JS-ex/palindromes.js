9d931b96b1084a857a4bd7eceb67d6150dc4c933
Vespid <shadifox@gmail.com>
2018-06-10 15:17:18 -0700

    first blogger commit

--- JS-ex/palindromes.js
+++
@@ -1,8 +0,0 @@
-var palindromes = function(str) {
-	var cleanStr=str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-	cleanStr=cleanStr.toLowerCase();
-	reverseStr = cleanStr.split("").reverse().join("");
-	return reverseStr===cleanStr
-}
-
-module.exports = palindromes
